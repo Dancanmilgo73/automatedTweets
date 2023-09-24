25ac4d6f09c552567ec548b59b8d6d7b4358c211
Dancan  Ngetich <dancan.ngetich@thejitu.com>
2023-09-24 13:54:45 +0300

    first install modules

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const openai = require('openai');
 const { TwitterApi } = require("twitter-api-v2");
 const CronJob = require('cron').CronJob;
